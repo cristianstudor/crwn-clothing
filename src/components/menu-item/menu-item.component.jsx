25c2997ee6d04a59f8f26e3eb19e949868b0b24c
cristianstudor <cristi_tcs@live.com>
2019-08-21 18:31:31 +0300

    updating app with shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -9,7 +9,7 @@ const MenuItem = ({title, imageUrl, size, history, match, linkUrl}) => (
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div 
-      className='background-image'  
+      className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }} 
