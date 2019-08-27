c0bfa3fcaf40c7a6e7ec1d7da28d3f67537ad1ea
cristianstudor <cristi_tcs@live.com>
2019-08-27 11:01:56 +0300

    added carTcon and cartDropdown components to the app, also implementes car reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartItem from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -29,12 +31,18 @@ const Header = ({ currentUser }) => (
           SIGN IN
        </Link>
       )}
+      <CartItem />
     </div>
+    { 
+      hidden ? null : 
+      <CartDropdown /> 
+    }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = state => ({   // const mapStateToProps = ({user:{ currentUser }, cart:{ hidden }}) => ({ 
+  currentUser: state.user.currentUser, // currentUser,
+  hidden: state.cart.hidden             // hidden
 })
 
 export default connect(mapStateToProps)(Header);
