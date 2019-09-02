8aeed91649f893c961950e04fb4c04259a952811
cristianstudor <cristi_tcs@live.com>
2019-09-02 11:00:04 +0300

    implement code that pulls in shop data from firestore and stores it into the shop reducer from our shop page. Also included WithSpinner HOC

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
